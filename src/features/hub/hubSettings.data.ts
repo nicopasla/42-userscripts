0f4a92f45f3b884f1ea150233bb7a46bb8e72d84
nicopasla <45522473+nicopasla@users.noreply.github.com>
2026-09-07 21:30:12 +0200

    chore(hub): rename color value from "Coffee" to "Aubergine"

--- src/features/hub/hubSettings.data.ts
+++ src/features/hub/hubSettings.data.ts
@@ -362,7 +362,7 @@ export const HUB_SETTING_DEFS: Record<FeatureId, readonly HubSettingDef[]> = {
         { label: "Cyberpunk", value: "cyberpunk", color: "341 100% 70%" },
         { label: "Arthur", value: "dim", color: "108 66% 73%" },
         { label: "Black", value: "black", color: "0 0% 23%" },
-        { label: "Coffee", value: "coffee", color: "30 66% 58%" },
+        { label: "Aubergine", value: "coffee", color: "30 66% 58%" },
         { label: "Aqua", value: "aqua", color: "182 90% 51%" },
         { divider: true },
         { label: "Light" },
